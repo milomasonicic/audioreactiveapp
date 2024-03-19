ed3c96fe0d2d3eecf3ad8abd3fb604384ed1e85a
milo.masonicic <ilom3774@gmail.com>
2024-03-19 22:54:59 +0100

    little bit of style

--- resources/js/Components/sliders/colorpicker.jsx
+++ resources/js/Components/sliders/colorpicker.jsx
@@ -18,10 +18,10 @@ export default function ColorPicker({changeColor, changeSecondColor}) {
              
             </div> 
 
-            <div className="w-full flex justify-between">
+            <div className="w-full flex justify-center">
 
-                <Button color="gray" onClick={()=>changeSecondColor(color) }>Change Color Bar 1</Button>
-                <Button color="gray" onClick={()=>changeSecondColor(color)}>Change Color Bar 2</Button>
+                <button type="button" onClick={()=>changeSecondColor(color)}   class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Change Color Bar 1</button>
+                <button type="button" onClick={()=>changeSecondColor(color)} class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Change Color Bar 2</button>
             
             </div>   
   
