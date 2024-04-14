d0917d164aa7ccb51930a0df31db58521fc0ec17
milo.masonicic <ilom3774@gmail.com>
2024-04-14 15:42:15 +0200

    single message view

--- resources/js/Pages/MessageSingle.jsx
+++ resources/js/Pages/MessageSingle.jsx
@@ -1,9 +1,36 @@
-export default function Single({message}) {
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
+export default function Single({message, auth}) {
 
     return (
         <div>
-            <h1>mesage</h1>
-          {message.content}
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">  </h2>}
+        >
+            <div className='pl-4 pt-4 max-w-[1240px] mx-auto'>
+                <h1 className='text-2xl font-semibold mb-2'>Name</h1>
+                <input type="text"
+                value={message.name}
+                readOnly
+                className='w-4/5 md:w-1/2 p-2 rounded bg-white-100'  />
+            
+                   
+                <h1 className='text-2xl font-semibold mt-2 mb-2'>Email</h1> 
+                <input type="text"
+                value= {message.email}
+                readOnly
+                className='w-4/5 md:w-1/2 p-2 rounded bg-white-100'  />
+                <h1 className='text-2xl font-semibold mb-2'>Message content</h1>
+               
+                <textarea
+                readOnly
+                className='w-4/5 md:w-1/2 p-2 rounded bg-white-100' 
+                rows='6'
+                value={message.content} />
+            </div>
+        
+        </AuthenticatedLayout>    
         </div>
     )
 }
