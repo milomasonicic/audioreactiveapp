95ca8df2b7cb010c5813385660d54712fc02acc9
milo.masonicic <ilom3774@gmail.com>
2024-04-15 20:08:22 +0200

    adding some style

--- resources/js/Components/audioreactive/songdatavizulizer.jsx
+++ resources/js/Components/audioreactive/songdatavizulizer.jsx
@@ -2,17 +2,6 @@ import { useRef, useState } from "react";
 
 import EditingSlider from '../sliders/slider'
 import CircularSlider from '@fseehawer/react-circular-slider';
-
-
-import Gradiantvalue from "../sliders/gradiantvalue";
-import GradiantvalueOne from  "../sliders/gradiantvalueone"
-import GradiantvalueTwo from "../sliders/gradiantvaluetwo"
-//import ColorPicker from '../sliders/colorpicker'
-
-import RotationSlider from "../sliders/circularSlider"
-import ShapeSlider from '../sliders/changeShape'
-import HeightSlider from '../sliders/heightSlider'
-
 import GradientPicker from "../sliders/colorpickergradinet";
 
 
@@ -122,15 +111,9 @@ export default function Num() {
     let start01 = 50
     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
     for (let i = 0; i < songData.length; i++) {
-      // compute x coordinate where we would draw
-      //start = i * 8;
+     
       startRef.current = i * 4 ;
-     // start01 = i * 50 
-
-     ///PROMIJENITI OVU 4
-  
-
-   // ctx.font = fontSize;
+     
     var fontSize1 = fontRef.current.toString()    
     ctx.font = fontSize1 + "px Arial"
     ctx.fillStyle = gradientcolorRef.current
@@ -146,10 +129,8 @@ export default function Num() {
     ctx.fillText(songData[i], 100, 100)
     ctx.fillStyle = gradientcolor2Ref.current;
     ctx.translate(startRef.current * bar_width, heightRef.current + -songData[i] * 0.9 )
-    ctx.fillText(songData[i], 50, 50)
-
-  //ctx.fillText(songData[i],  startRef.current * bar_width, heightRef.current + -songData[i] * 0.2 );
-     ctx.restore() 
+    ctx.fillText(songData[i], 50, 50) 
+    ctx.restore() 
 
      
      
@@ -159,9 +140,9 @@ export default function Num() {
   
 
   return (
-    <div className="App flex">
-      <div className="w-[800px] bg-red-700 flex flex-col h-[600px]">
-      <div className="border border-black w-full order-last mx-auto flex-col h-[100px]">
+    <div className="App flex flex-col md:flex-row bg-gray-50 md:h-[590px] h-[990px]">
+      <div className="max-w-[640px] mx-auto  flex flex-col pt-4">
+      <div className="w-full order-last mx-auto h-[100px]">
       <input
         type="file"
         onChange={({ target: { files } }) => files[0] && setFile(files[0])}
@@ -178,49 +159,57 @@ export default function Num() {
       </div>       
    
 
-      <canvas ref={canvasRef} width={790} height={420}  className="bg-white mx-auto border border-red-700"/>
+      <canvas ref={canvasRef} className="max-w-[98%] h-[300px] md:h-[400px] bg-white border border-black mx-auto"/>
       </div>  
 
 
         
+      <div  className="max-w-[640px] mx-auto h-[250px] flex-col pt-8" >
+    
       <div>
-
-      <GradientPicker changeGradient={changeGradient} changeGradientOne={changeGradientOne} changeGradientTwo={changeGradientTwo} ></GradientPicker>
-      <div>
-        
-      
-        <h1>bars</h1>
 
         <EditingSlider title={"Density"} min={2} max={8} OnChangeEventTriggerd={OnChangeEventTriggerd}></EditingSlider>
         <EditingSlider title={"Height"} min={10} max={400} OnChangeEventTriggerd={OnChangeHeight}></EditingSlider>
         
 
-        <h1>circular</h1>
-
-        <h1> Angle</h1>
+      <div className="flex pt-4 pb-4">
+        <div className="mr-4">
         <CircularSlider 
-
-        width={100}
-        height={100}
+        label=" "
+        valueFontSize= "0 rem"
+        center={null}
+        width={70}
+        height={70}
+        knobPosition={0}
+        knobSize= {12}
         min={1}
         max={360}
         onChange={(value) => changeAngle(value)}></CircularSlider>
          
-        <h1>Font</h1>
+
+        </div>
+     
 
         <CircularSlider 
-
-          width={100}
-          height={100}
+          label=" "
+          valueFontSize= "0 rem"
+          knobSize= {12}
+          width={70}
+          height={70}
+          knobPosition={0}
           min={14}
           max={72}
           onChange={(value) => changeFontSize(value)}></CircularSlider>
+
+      </div>
+
        
-     
+      </div>
+      <div className="mt-10 md:mt-4">
+        <GradientPicker changeGradient={changeGradient} changeGradientOne={changeGradientOne} changeGradientTwo={changeGradientTwo} ></GradientPicker>
+      </div>
  
-</div>
- 
-</div>
+    </div>
      
       
 
--- resources/js/Pages/Numbers.jsx
+++ resources/js/Pages/Numbers.jsx
@@ -1,13 +1,23 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-
+import Nav from '@/Components/Nav';
 import Num from   '@/Components/audioreactive/songdatavizulizer'
-
+import Footer from '@/Components/landingpage/footer';
 
 export default function Numbers() {
     return (
        <div>
-       <Num></Num>
+        <div className='bg-gray-100 pb-[150px]'>
+        <Nav></Nav>
+        <div className='mt-[25px] max-w-container mx-auto border border-white-700 rounded-3xl pt-12 pb-12 bg-gray-50'>
+        <   Num></Num>
+        </div>
+    
+        </div>
+
+        <div className=''>
+            <Footer></Footer>   
+        </div>
       
        </div>
     );
